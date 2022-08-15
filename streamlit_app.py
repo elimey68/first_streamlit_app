3a521d6091837cfd3234e9028307d4ab473c6572
elimey68 <110379862+elimey68@users.noreply.github.com>
2022-08-15 15:40:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.title('Breakfast Menue')
+streamlit.text('Omega 3 and Bluenerry Muffines')
+streamlit.text('kale, Spinach & Rocket Smoothie')
+
